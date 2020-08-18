19cad3dae8a6b5441d70bac4612f5da41df11eb9
Nicola Peduzzi <thenikso@gmail.com>
2020-08-18 14:42:13 +0200

    Create bet contract

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -8,4 +8,20 @@ contract Bet is Ownable, Destroyable {
   function addFunds() public payable ownerOnly returns(uint) {
     return address(this).balance;
   }
+
+  uint private nonce = 0;
+
+  function flipCoin() public payable returns(uint) {
+    require(msg.value > 0, "Needs an ETH amount to bet");
+    uint maxBet = (address(this).balance - msg.value) / 2;
+    require(msg.value <= maxBet, "Not enought funds to accept bet");
+    // TODO make this actual random
+    uint flip = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, nonce))) % 2;
+    if (flip == 1) {
+      uint wonBet = msg.value * 2;
+      msg.sender.transfer(wonBet);
+      return wonBet;
+    }
+    return 0;
+  }
 }
--- test/Bet.test.js
+++ test/Bet.test.js
@@ -2,7 +2,25 @@ const truffleAssert = require('truffle-assertions');
 const Bet = artifacts.require('Bet');
 const BN = web3.utils.BN;
 
+if (typeof Number.prototype.ether === 'undefined') {
+  Object.defineProperties(Number.prototype, {
+    gwei: {
+      get: function () {
+        return web3.utils.toWei(String(this), 'gwei');
+      },
+    },
+    ether: {
+      get: function () {
+        return web3.utils.toWei(String(this), 'ether');
+      },
+    },
+  });
+}
+
 contract('Bet', (accounts) => {
+  const owner = accounts[0];
+  const guest = accounts[1];
+
   after(async () => {
     const bet = await Bet.deployed();
     await bet.destroy();
@@ -11,11 +29,7 @@ contract('Bet', (accounts) => {
   it('should have 1 ETH when deployed', async () => {
     const bet = await Bet.deployed();
     const balance = await web3.eth.getBalance(bet.address);
-    assert.equal(
-      balance,
-      web3.utils.toWei('1', 'ether'),
-      '1 ETH not in the contract balance',
-    );
+    assert.equal(balance, (1).ether, '1 ETH not in the contract balance');
   });
 
   it('should not addFunds as non owner', async () => {
@@ -29,8 +43,35 @@ contract('Bet', (accounts) => {
     const bet = await Bet.deployed();
     const initialBalance = await web3.eth.getBalance(bet.address);
     const value = 10000;
-    await truffleAssert.passes(bet.addFunds({ value, from: accounts[0] }));
+    await truffleAssert.passes(bet.addFunds({ value, from: owner }));
     const balance = await web3.eth.getBalance(bet.address);
     assert.equal(balance, new BN(initialBalance).add(new BN(value)));
+  });
+
+  it('should NOT flipCoin if no value is sent', async () => {
+    const bet = await Bet.deployed();
+    await truffleAssert.fails(bet.flipCoin({ value: 0, from: guest }));
+  });
+
+  it('should NOT flipCoin if value can not be doubled', async () => {
+    const bet = await Bet.deployed();
+    const balance = await web3.eth.getBalance(bet.address);
+    const value = new BN(balance).div(new BN(2)).add(new BN(1)).toString();
+    await truffleAssert.fails(bet.flipCoin({ value, from: guest }));
+  });
+
+  it('should flipCoin and double or zero gets money', async () => {
+    const bet = await Bet.deployed();
+    const value = (1000).gwei;
+    const results = await Promise.all(
+      Array.from({ length: 6 }).map((_, i) =>
+        bet.flipCoin.call({ value, from: accounts[i + 1] }),
+      ),
+    );
+    const zero = new BN(0);
+    const won = results.filter((v) => v.gt(zero));
+    const lost = results.filter((v) => v.eq(zero));
+    assert(won.length > 0, 'Nothing was won!');
+    assert(lost.length > 0, 'Nothing was lost!');
   });
 });
