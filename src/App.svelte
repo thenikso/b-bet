5cf92a2856702f63a61b2ebf9ece6505d6d9fd52
Nicola Peduzzi <thenikso@gmail.com>
2020-08-18 22:47:39 +0200

    Add style

--- src/App.svelte
+++ src/App.svelte
@@ -6,8 +6,9 @@
   import web3 from "./web3.js";
 
   let betContract;
-  let betAmount = 0;
+  let betAmount;
   let betHead = true;
+  let betting = false;
 
   $: maxBet = Math.min($contractBalance / 2, $accountBalance);
 
@@ -16,13 +17,26 @@
   }
 
   async function flipCoin() {
-    // TODO validate inputs
+    if (betAmount <= 0 || betAmount > maxBet) {
+      return;
+    }
+    betting = true;
     const value = web3.utils.toWei(String(betAmount), "ether");
     console.log("Begging", value);
-    const result = await $contract.methods
-      .flipCoin(betHead ? 1 : 0)
-      .send({ value });
-    console.log("win:", result.events.coinFlipped.returnValues.won !== "0");
+    try {
+      const result = await $contract.methods
+        .flipCoin(betHead ? 1 : 0)
+        .send({ value });
+      const wonAmount = web3.utils.fromWei(
+        result.events.coinFlipped.returnValues.won,
+        "ether"
+      );
+      betAmount = undefined;
+      betting = false;
+      console.log("win:", wonAmount !== "0");
+    } catch (e) {
+      betting = false;
+    }
   }
 </script>
 
@@ -30,14 +44,13 @@
   main {
     width: 100%;
     height: 100%;
-    color: white;
-    background: rgb(32, 14, 49);
-    background: linear-gradient(
-      135deg,
-      rgba(32, 14, 49, 1) 0%,
-      rgba(58, 10, 79, 1) 50%,
-      rgba(32, 14, 49, 1) 100%
+    background-image: radial-gradient(
+      50% 50% at 50% 50%,
+      rgba(255, 0, 122, 0.1) 0%,
+      rgba(255, 255, 255, 0) 100%
     );
+    background-position: 0px -30vh;
+    background-repeat: no-repeat;
   }
 
   header {
@@ -47,9 +60,145 @@
   }
 
   .title {
+    font-family: "Concert One";
     text-align: center;
-    color: #c443a8;
+    color: rgb(230, 0, 110);
     font-size: 60px;
+  }
+
+  .bet-form {
+    margin: 50px auto;
+
+    position: relative;
+    max-width: 420px;
+    width: 100%;
+    box-shadow: rgba(0, 0, 0, 0.01) 0px 0px 1px, rgba(0, 0, 0, 0.04) 0px 4px 8px,
+      rgba(0, 0, 0, 0.04) 0px 16px 24px, rgba(0, 0, 0, 0.01) 0px 24px 32px;
+    opacity: 1;
+    background: rgb(255, 255, 255);
+    border-radius: 30px;
+    padding: 1rem;
+  }
+
+  .input-wrapper {
+    background-color: rgb(255, 255, 255);
+    border-radius: 20px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgb(247, 248, 250);
+    border-image: initial;
+  }
+
+  .input-wrapper label {
+    display: block;
+    padding: 0.75rem 1rem 0px;
+    font-weight: 500;
+    font-size: 14px;
+    color: rgb(86, 90, 105);
+    margin: 0px;
+  }
+
+  .input-field {
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    flex-flow: row nowrap;
+    padding: 0.75rem 0.75rem 0.75rem 1rem;
+  }
+
+  .input-field input {
+    color: rgb(0, 0, 0);
+    width: 0px;
+    position: relative;
+    font-weight: 500;
+    background-color: rgb(255, 255, 255);
+    font-size: 24px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    appearance: textfield;
+    outline: none;
+    border-width: initial;
+    border-style: none;
+    border-color: initial;
+    border-image: initial;
+    flex: 1 1 auto;
+    overflow: hidden;
+    padding: 0px;
+  }
+
+  .input-field .currency {
+    font-weight: 500;
+    font-size: 24px;
+    padding: 0 10px;
+  }
+
+  .bet-form button {
+    outline: none;
+    cursor: pointer;
+
+    border: none;
+    border-radius: 6px;
+    background-color: rgb(253, 234, 241);
+    color: rgb(255, 0, 122);
+    padding: 5px;
+
+    text-align: center;
+    font-weight: 500;
+    font-size: 14px;
+  }
+
+  .bet-form button:hover:not(:disabled) {
+    background-color: rgb(252, 220, 232);
+  }
+
+  .bet-form button:disabled {
+    opacity: 0.5;
+  }
+
+  .bet-form button.callout {
+    width: 100%;
+    padding: 18px;
+    font-size: 18px;
+    border-radius: 18px;
+  }
+
+  .bet-form button.outline {
+    outline: none;
+    cursor: pointer;
+
+    border: 1px solid rgb(255, 0, 122);
+    border-radius: 6px;
+    background: none;
+    color: rgb(255, 0, 122);
+    padding: 5px;
+
+    font-weight: 500;
+    font-size: 14px;
+  }
+
+  .bet-form button.outline:hover {
+    border-color: rgb(230, 0, 110);
+    color: rgb(230, 0, 110);
+  }
+
+  .bet-form button.select {
+    flex-grow: 1;
+    font-size: 24px;
+    border: 1px solid rgb(218, 218, 218);
+    border-radius: 12px;
+    background: none;
+    color: rgb(168, 168, 168);
+    padding: 5px;
+    margin: 0 5px;
+  }
+
+  .bet-form button.select.selected {
+    color: white;
+    background-color: rgb(218, 218, 218);
+  }
+
+  .bet-form .sep {
+    padding: 0.5rem 1rem;
   }
 </style>
 
@@ -60,8 +209,80 @@
     </ConnectMetamask>
   </header>
   <h1 class="title">You Bet!</h1>
-  <input type="number" max={maxBet} step="0.1" bind:value={betAmount} />
-  <input type="checkbox" bind:checked={betHead} />
-  <button type="button" on:click={flipCoin}>Flip!</button>
+  <div class="bet-form">
+    <div class="input-wrapper">
+      <label for="amount">Bet amount</label>
+      <div class="input-field">
+        <input
+          id="amount"
+          type="number"
+          min="0"
+          max={maxBet}
+          step="0.01"
+          inputmode="decimal"
+          pattern="^[0-9]*[.,]?[0-9]*$"
+          bind:value={betAmount}
+          placeholder="0.0"
+          spellcheck="false"
+          autocorrect="off"
+          autocomplete="off" />
+        <span class="currency">ETH</span>
+        <button
+          type="button"
+          class="outline"
+          on:click={() => {
+            betAmount = maxBet;
+          }}>
+          MAX
+        </button>
+      </div>
+    </div>
+    <div class="sep">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#565A69"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round">
+        <line x1="12" y1="5" x2="12" y2="19" />
+        <polyline points="19 12 12 19 5 12" />
+      </svg>
+    </div>
+    <div class="input-wrapper">
+      <label for="head">Bet on</label>
+      <div class="input-field">
+        <button
+          type="button"
+          class="select"
+          class:selected={betHead}
+          on:click={() => {
+            betHead = true;
+          }}>
+          HEAD
+        </button>
+        <button
+          type="button"
+          class="select"
+          class:selected={!betHead}
+          on:click={() => {
+            betHead = false;
+          }}>
+          TAIL
+        </button>
+      </div>
+    </div>
+    <div class="sep" />
+    <button
+      type="button"
+      class="callout"
+      on:click={flipCoin}
+      disabled={betting}>
+      Place bet
+    </button>
+  </div>
 
 </main>
